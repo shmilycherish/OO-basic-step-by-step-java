5ecde7396671791f380233b8288a867b3892afa1
cyang <shmilycherish@gmail.com>
2019-07-23 19:32:12 +0800

    add constructor and get method for student in practice 2

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,26 @@
 package practice02;
 
 public class Student {
+    private final String name;
+    private final int age;
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
