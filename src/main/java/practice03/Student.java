83c1b6edde6a6655b2b25509a1f186b38ebc7617
cyang <shmilycherish@gmail.com>
2019-07-23 19:36:13 +0800

    implement person and student in practice 3

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,30 @@
 package practice03;
 
 public class Student {
+    private final String name;
+    private final int age;
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %s.", getKlass());
+    }
 }
