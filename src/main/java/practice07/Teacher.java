2cb113ce63ed5fc791cb32f18c891f18db9df231
cyang <shmilycherish@gmail.com>
2019-07-24 19:57:56 +0800

    implement teacher in practice 7

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + detailIntroduce();
+    }
+
+    private String detailIntroduce() {
+        return getKlass() == null ? " I am a Teacher. I teach No Class." :
+                String.format(" I am a Teacher. I teach %s.", getKlass().getDisplayName());
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == getKlass().getNumber()) {
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
+
+
 }
