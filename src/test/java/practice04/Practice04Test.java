6214da3212ae7dbcc885e5fe14e748f73cdc351c
cyang <shmilycherish@gmail.com>
2019-07-24 19:12:19 +0800

    implement student in practice 4

--- src/test/java/practice04/Practice04Test.java
+++ src/test/java/practice04/Practice04Test.java
@@ -19,20 +19,20 @@ public class Practice04Test {
         assertThat(introduce).isEqualTo("My name is Tom. I am 21 years old.");
     }
 
-//    @Test
-//    public void should_student_have_name_age_and_class_number() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.getName()).isEqualTo("Tom");
-//        assertThat(tom.getAge()).isEqualTo(21);
-//        assertThat(tom.getKlass()).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void should_student_introduce_with_class() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
-//    }
-//
+    @Test
+    public void should_student_have_name_age_and_class_number() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.getName()).isEqualTo("Tom");
+        assertThat(tom.getAge()).isEqualTo(21);
+        assertThat(tom.getKlass()).isEqualTo(2);
+    }
+
+    @Test
+    public void should_student_introduce_with_class() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
+    }
+
 //    @Test
 //    public void should_worker_have_name_and_age() throws Exception {
 //        Worker tom = new Worker("Tom", 21);
