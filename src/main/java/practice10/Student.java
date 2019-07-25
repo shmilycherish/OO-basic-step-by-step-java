1a09b58c268c0ca77e0c76163cd69b996f3d7e68
cyang <shmilycherish@gmail.com>
2019-07-25 12:06:25 +0800

    implement person, klass and student in practice 10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,24 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private final Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + detailIntroduce();
+    }
+
+    private String detailIntroduce() {
+        return this.equals(klass.getLeader()) ? String.format(" I am a Student. I am Leader of %s.", getKlass().getDisplayName()) :
+                String.format(" I am a Student. I am at %s.", getKlass().getDisplayName());
+    }
 }
