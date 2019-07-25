f94345a27da9e690783734554daccb15431f0613
cyang <shmilycherish@gmail.com>
2019-07-25 11:01:36 +0800

    implement person, kclass and student in practice 8

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,26 @@
 package practice09;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        System.out.println("It is not one of us.");
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
