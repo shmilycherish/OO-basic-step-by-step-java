951f11aba673c36f1820a37ce14a1270f7116065
cyang <shmilycherish@gmail.com>
2019-07-25 13:42:11 +0800

    implement teacher in practice 10

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,47 @@
 package practice10;
 
-public class Teacher {
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private List<Klass> classes;
+
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public boolean isTeaching(Student student) {
+        return classes.stream().anyMatch(klass -> klass.getNumber() == student.getKlass().getNumber());
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + detailIntroduce();
+    }
+
+    private String detailIntroduce() {
+        return getClasses() == null || getClasses().size() == 0 ? " I am a Teacher. I teach No Class." :
+                String.format(" I am a Teacher. I teach Class %s.", formatClassNumber());
+    }
+
+    private String formatClassNumber() {
+      return getClasses().stream().map(klass -> String.valueOf(klass.getNumber())).collect(Collectors.joining(", "));
+    }
+
+    public String introduceWith(Student student) {
+        if(isTeaching(student)) {
+            return super.introduce() + String.format(" I am a Teacher. I teach %s.", student.getName());
+        }
+        return super.introduce() + String.format(" I am a Teacher. I don't teach %s.", student.getName());
+    }
+
 }
